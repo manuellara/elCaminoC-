71be50c2c6dd7238376da70cec52eebbf84480cd
Manuel Lara <manuellaraa@gmail.com>
2017-02-22 15:27:54 -0800

    first assignment

--- cppSample1/cppSample1/Source.cpp
+++ cppSample1/cppSample1/Source.cpp
@@ -1,7 +1,32 @@
+//-----------------
+//Test1.cpp
+//Manuel Lara
+//-----------------
+
+
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main()
 {
-	cout << ("Hello, World") << endl;
+	cout << "Manuel Lara" << endl;
+
+	string Name;
+	int Year;
+
+	cout << "Please enter you name and press enter: ";
+	getline (cin, Name);
+
+	cout << "Please enter your birth year using 4 digits: ";
+	cin >> Year;
+
+	cout << "\n\n\aHello, " << Name << "! Welcome to CS 1. \n\n\a" << endl;
+
+	cout << "At age of  " << 2017 - Year << ", you will have a fun semester.";
+
+	cout << "\n\nHave a good day. \n\n";
+
+	return 0;
 }
