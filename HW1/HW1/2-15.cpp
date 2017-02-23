fcd0fc45b19004d4e0c8d43b92fce4f6b76cee9b
Manuel Lara <manuellaraa@gmail.com>
2017-02-22 16:40:33 -0800

    hw1

---
+++ HW1/HW1/2-15.cpp
@@ -0,0 +1,22 @@
+//-------------
+//2-15 HW1
+//Manuel Lara
+//--------------
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Manuel Lara" << endl;
+
+	string movieTitle;
+
+	movieTitle = "Wheels of Fury";
+
+	cout << "My favorite movie is " << movieTitle << endl;
+
+	return 0;
+}
