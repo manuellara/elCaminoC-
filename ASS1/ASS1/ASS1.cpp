44cce34039fe67c9734abc7fadfaa85a0c40a675
Manuel Lara <manuellaraa@gmail.com>
2017-04-15 12:33:20 -0700

    ammortization

--- ASS1/ASS1/ASS1.cpp
+++ ASS1/ASS1/ASS1.cpp
@@ -4,6 +4,55 @@ Ammortization Table
 
 Algorithm 
 
+STEP 1 : INPUT
+import all the libraries you will need, in this case we used iostream, sring, cmath, and iomanip
+next, initialize variables for name, choice, loan amount, interest, and years to pay
+
+next prompt the user to enter thir name and store it in the name variable
+validate the input
+
+next, promp the user to enter whether they would like to create an ammortization table(y or n) and store answer in the choice variable
+evaluate the input
+if it is neither 'y' or 'n', it will prompt you to reenter a value 
+if 'n', program will exit
+if 'y' program will proceed
+
+next, prompt the user to enter a loan amount and store it in the loam amount variable
+evaluate input
+if loan amount is not a number, prompt the user  to reenter a number
+
+next, prompt the user to enter an interest rate and store it in the interest rate variable
+evaluate input 
+if interest rate is not a n umber, prompt the user to reenter a valid interest rate
+once a valid number is read, divide the number by 100 to get the decimal and store it in the interest rate variable
+
+next, prompt the user to enter amount of years they would like to pay off the loan and store it in years to pay variable 
+evaluate input
+if years to pay is not a number, prompt the user to enter a valid number
+
+STEP 2 : PROCESS 
+set user input to accomidate ammortization formula variables
+P = loan amount 
+n = years to pay * 12
+r = interest / n
+base = (1+r)^n
+A = P * ( base*r / base-1 )
+
+next, include print statements to show all user input
+
+STEP 3 : OUTPUT
+include a standalone print staement the catagory labels (payment no, payment amount, interest, principal, ending balance)
+next we will create a loop to print the output, but first we must initialize a local variable and set it to the ending balance OUTSIDE of the loop
+
+create a loop and set the counter to stop at our previously defined n variable + 1 
+to make things more readable
+set A to mp
+set inter to ending * r
+set principal to mp - inter
+
+next, have your print statement 
+
+lastly, set the reset variable to ending to ending - principal 
 */
 
 #include <iostream>
